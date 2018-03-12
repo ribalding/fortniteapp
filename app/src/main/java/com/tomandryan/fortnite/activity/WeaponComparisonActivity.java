e4b4a7262874988e1729422c5ce10842100f489b
TomVrabel <tomvrabel1@gmail.com>
2018-03-11 23:36:40 -0400

    Weapon-Comparison-Tool: Added two new weapon spinners, set up preliminary onItemSelectListener for weapon type.

--- app/src/main/java/com/tomandryan/fortnite/activity/WeaponComparisonActivity.java
+++ app/src/main/java/com/tomandryan/fortnite/activity/WeaponComparisonActivity.java
@@ -2,35 +2,61 @@ package com.tomandryan.fortnite.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 import com.tomandryan.fortnite.R;
-import com.tomandryan.fortnite.enums.WeaponTypes;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.EnumSet;
 import java.util.List;
 
 public class WeaponComparisonActivity extends AppCompatActivity {
     @BindView(R.id.weaponTypeA) Spinner weaponTypeA;
     @BindView(R.id.weaponRarityA) Spinner weaponRarityA;
+    @BindView(R.id.weaponTypeB) Spinner weaponTypeB;
+    @BindView(R.id.weaponRarityB) Spinner weaponRarityB;
+    @BindView(R.id.weaponSelectionA) TextView weaponSelectionA;
+
+    final static List<String> ALL_WEAPON_TYPES = Arrays.asList("Assault Rifle", "Scoped Assault Rifle", "Burst Assault Rifle", "Suppressed Submachine Gun", "Tactical Submachine Gun",
+            "Minigun", "Hand Cannon", "Pistol", "Revolver", "Suppressed Pistol", "Bolt-Action Sniper Rifle", "Hunting Rifle", "Semi-Auto Sniper Rifle",
+            "Pump Action Shotgun", "Tactical Shotgun", "Grenade Launcher", "Rocket Launcher", "Crossbow");
+
+    final static List<String> ALL_WEAPON_RARITIES = Arrays.asList("Grey (Uncommon)", "Green (Uncommon)", "Blue (Rare)", "Purple (Epic)", "Gold (Legendary)");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.weapon_comparison_activity);
 
         ButterKnife.bind(this);
 
-        List<String> allWeaponTypes = Arrays.asList("Pump Action Shotgun", "Tactical Shotgun", "Silenced Pistol");
-        ArrayAdapter<String> weaponTypeAdapter = new ArrayAdapter<String>(WeaponComparisonActivity.this, android.R.layout.simple_spinner_dropdown_item, allWeaponTypes);
+        ArrayAdapter<String> weaponTypeAdapter = new ArrayAdapter<String>(WeaponComparisonActivity.this, android.R.layout.simple_spinner_dropdown_item, ALL_WEAPON_TYPES);
         weaponTypeA.setAdapter(weaponTypeAdapter);
+        weaponTypeB.setAdapter(weaponTypeAdapter);
 
+        ArrayAdapter<String> weaponRarityAdapter = new ArrayAdapter<String>(WeaponComparisonActivity.this, android.R.layout.simple_spinner_dropdown_item, ALL_WEAPON_RARITIES);
+        weaponRarityA.setAdapter(weaponRarityAdapter);
+        weaponRarityB.setAdapter(weaponRarityAdapter);
+
+        weaponTypeA.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                weaponSelectionA.setText(ALL_WEAPON_TYPES.get(i));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                /* look into putting a default text on nothing selected */
+            }
+        });
 
     }
+
+
 }
