185b09da97ffc6f09be960b6730251f4186d6f82
TomVrabel <tomvrabel1@gmail.com>
2018-04-14 12:05:30 -0400

    Weapon-Comparison-Tool: center justified potential weapon stats, added percentagePositiveTest, realligned headshot damage below headshot and made size adjustments in the weapon_comparison_activity.xml

--- app/src/test/java/com/tomandryan/fortnite/CompareWeaponStatsTest.java
+++ app/src/test/java/com/tomandryan/fortnite/CompareWeaponStatsTest.java
@@ -1,5 +1,7 @@
 package com.tomandryan.fortnite;
 
+import android.graphics.Color;
+
 import com.tomandryan.fortnite.model.Weapons;
 
 import org.junit.Test;
@@ -8,13 +10,18 @@ import static org.junit.Assert.*;
 
 public class CompareWeaponStatsTest {
 
-    public CompareWeaponStatsTest(){
+    public CompareWeaponStatsTest() {
     }
 
     @Test
-    public void compareWeaponStatsTest(){
+    public void compareWeaponStatsTest() {
         int comparisonResultTest = Weapons.compareWeaponStats(90.00, 120.00);
-        assertEquals("compareWeaponStats method correctly returns the percentage difference between two numbers",33, comparisonResultTest);
+        assertEquals("compareWeaponStats method correctly returns the percentage difference between two numbers", 33, comparisonResultTest);
+    }
+
+    @Test
+    public void percentagePositiveTest() {
+        int percentagePositiveResultTest = Weapons.percentagePositive(10);
+        assertEquals("percentagePositive method correctly changes the color of the percent change", Color.GREEN, percentagePositiveResultTest);
     }
 }
-
